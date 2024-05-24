7d5e09f41f012388a8a5bcc2ca325b82091a1713
Bharat Ruidas <74824417+brd2002@users.noreply.github.com>
2024-05-24 19:54:49 +0530

    leture 4 completed

--- tut1.js
+++ tut1.js
@@ -1,2 +1,11 @@
-console.log("what is your name tell me ? ")
-console.log("Please bharat tell me what your name tell me ? ")
+// console.log("what is your name tell me ? ")
+// console.log("Please bharat tell me what your name tell me ? ")
+// console.log("what your name tell me ? 😊")
+const accountId = "1234567"
+// console.log(accountId)
+var name = "bharat" 
+var collegeName = "sanaka educational trust"
+let engineeringBranch = "Computer science engineering"
+console.table([
+    accountId , name , collegeName , engineeringBranch
+])
