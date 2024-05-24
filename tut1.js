4ec8339a81e8b73827e9078e8d1d68fe3be323ce
Bharat Ruidas <74824417+brd2002@users.noreply.github.com>
2024-05-24 19:56:35 +0530

    this is lecture 4 last commit

--- tut1.js
+++ tut1.js
@@ -3,8 +3,9 @@
 // console.log("what your name tell me ? 😊")
 const accountId = "1234567"
 // console.log(accountId)
-var name = "bharat" 
-var collegeName = "sanaka educational trust"
+// please don't use var 
+let  name = "bharat" 
+let  collegeName = "sanaka educational trust"
 let engineeringBranch = "Computer science engineering"
 console.table([
     accountId , name , collegeName , engineeringBranch
