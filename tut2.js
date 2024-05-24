191573305de7cb95a4e42a06b4e726af335d9269
Bharat Ruidas <74824417+brd2002@users.noreply.github.com>
2024-05-24 20:21:43 +0530

    lecture 5 completed

--- tut2.js
+++ tut2.js
@@ -1,1 +1,8 @@
 "use strict"; // treat all js code as a newer version
+// null -> stand alone value , type is  object
+// undifined -> for now we are not asign the value , it's an type
+// symbol -> unique 
+
+// object 
+
+console.log(typeof undefined)
