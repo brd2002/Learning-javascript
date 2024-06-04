b75d89b696a2a3351d0557c7b4baee383fa05866
Bharat Ruidas <74824417+brd2002@users.noreply.github.com>
2024-06-04 16:34:29 +0530

    this is 5 minutes break

--- Complete learning on javascript/tutorial1.js
+++ Complete learning on javascript/tutorial1.js
@@ -1,7 +1,13 @@
-const accoutId = 123
-let accountEmail = "bharat@gmail.com"
-var accountCity = "kolkata"
-console.log(accoutId)
-console.log(accountEmail)
-console.log(accountCity)
-console.table([accountCity , accountEmail , accoutId])
+"use strict"
+//const accoutId = 123
+//let accountEmail = "bharat@gmail.com"
+//var accountCity = "kolkata"
+//console.log(accoutId)
+//console.log(accountEmail)
+//console.log(accountCity)
+//console.table([accountCity , accountEmail , accoutId])
+//alert("hello bharat")
+//console.log(typeof null)
+//let counter = 1
+//console.log(counter++)
+//console.log(++counter)
