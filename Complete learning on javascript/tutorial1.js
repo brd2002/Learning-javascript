206131f2be3454844c92b2c473cb0dde41ff8859
Bharat Ruidas <74824417+brd2002@users.noreply.github.com>
2024-06-09 13:56:20 +0530

    for now break

--- Complete learning on javascript/tutorial1.js
+++ Complete learning on javascript/tutorial1.js
@@ -1,66 +1,70 @@
-"use strict"
-//const accoutId = 123
-//let accountEmail = "bharat@gmail.com"
-//var accountCity = "kolkata"
-//console.log(accoutId)
-//console.log(accountEmail)
-//console.log(accountCity)
-//console.table([accountCity , accountEmail , accoutId])
-//alert("hello bharat")
-//console.log(typeof null)
-//let counter = 1
-//console.log(counter++)
-//console.log(++counter)
-//let anu = accountEmail.toLocaleLowerCase()
-//console.log(anu)
-//console.log(`this is my ${accoutId} and also my account email is ${accountEmail}`)
-//const score  = new Number(200)
-//console.log(score)
-//let number = Number.MAX_VALUE
-//console.log(number.toLocaleString('en-IN')) this is using -> the indian number system
-//let num1 = 2.621334
-//console.log(Math.round(num1))
-// math.round it's work like a math when the value of precision is greater
-// than 5 or equal then it's give the ceil value of the number
-//let date = new Date
-//let stringDate = date.toLocaleDateString()
-//let allDateInfo = stringDate.split(" ")
-//console.log(stringDate)
-//const myArray = [1,2,3,4,5,19,10,7,2]
-//console.log(myArray.sort((a, b) => a > b ))
-const appUser = new Object() // this is a single tone object
-//console.log(appUser)
-//console.log(appUser.hasOwnProperty('isLogged'))
-function sayMyname() {
-    console.log("My name is bharat")
-}
+// "use strict"
+// //const accoutId = 123
+// //let accountEmail = "bharat@gmail.com"
+// //var accountCity = "kolkata"
+// //console.log(accoutId)
+// //console.log(accountEmail)
+// //console.log(accountCity)
+// //console.table([accountCity , accountEmail , accoutId])
+// //alert("hello bharat")
+// //console.log(typeof null)
+// //let counter = 1
+// //console.log(counter++)
+// //console.log(++counter)
+// //let anu = accountEmail.toLocaleLowerCase()
+// //console.log(anu)
+// //console.log(`this is my ${accoutId} and also my account email is ${accountEmail}`)
+// //const score  = new Number(200)
+// //console.log(score)
+// //let number = Number.MAX_VALUE
+// //console.log(number.toLocaleString('en-IN')) this is using -> the indian number system
+// //let num1 = 2.621334
+// //console.log(Math.round(num1))
+// // math.round it's work like a math when the value of precision is greater
+// // than 5 or equal then it's give the ceil value of the number
+// //let date = new Date
+// //let stringDate = date.toLocaleDateString()
+// //let allDateInfo = stringDate.split(" ")
+// //console.log(stringDate)
+// //const myArray = [1,2,3,4,5,19,10,7,2]
+// //console.log(myArray.sort((a, b) => a > b ))
+// const appUser = new Object() // this is a single tone object
+// //console.log(appUser)
+// //console.log(appUser.hasOwnProperty('isLogged'))
+// function sayMyname() {
+//     console.log("My name is bharat")
+// }
 
-function addTwoNumbers(number1, number2) {
-//    console.log(typeof number1)
-//    console.log(typeof number2)
-    if (typeof number1 === "number" && typeof number2 === "number") {
-        let result = (number1 + number2)
-        return result
-    }
-//    console.log("this function is running......")
-}
+// function addTwoNumbers(number1, number2) {
+// //    console.log(typeof number1)
+// //    console.log(typeof number2)
+//     if (typeof number1 === "number" && typeof number2 === "number") {
+//         let result = (number1 + number2)
+//         return result
+//     }
+// //    console.log("this function is running......")
+// }
 
-//addTwoNumbers("a" ,"c")
-for (let i = 0; i < 10; i++) {
-//    console.log(i)
-}
-const addThreeNumbers = (number1, number2, number3) => {
-    console.log(number1 + number2 + number3)
-}
-//addThreeNumbers(1,2,3)
-//Imidiately invoked function expression iife
-(()=> {
-    console.log(`DB connected`)
-})() ; 
-(()=> {
-    console.log(`DB connected two`)
-})();
-function bharat(){
-    console.log("What is your name bharat ? ")
-}
-bharat()
+// //addTwoNumbers("a" ,"c")
+// for (let i = 0; i < 10; i++) {
+// //    console.log(i)
+// }
+// const addThreeNumbers = (number1, number2, number3) => {
+//     console.log(number1 + number2 + number3)
+// }
+// //addThreeNumbers(1,2,3)
+// //Imidiately invoked function expression iife
+// (()=> {
+//     console.log(`DB connected`)
+// })() ;
+// (()=> {
+//     console.log(`DB connected two`)
+// })();
+// function bharat(){
+//     console.log("What is your name bharat ? ")
+// }
+// bharat()
+//const emptyObject = {"name" : "Bharat ruidas"}
+//if (Object.keys(emptyObject).length === 1){
+//    console.log("this is en empty object.....")
+//}
